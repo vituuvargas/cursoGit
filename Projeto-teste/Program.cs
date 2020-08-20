dcccd6f7d38b6ffd098a176ea2ca83ad30e53a5d
Vítor Vargas <vituvargass@gmail.com>
2020-08-20 16:39:14 -0300

    Projeto iniciado olá mundo

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo !");
+            int x = 1;
+            int y = 5;
+            int z = 10;
+
+
+            
+            Console.WriteLine((z > z) || (z == z) && (z < z));
+            Console.ReadLine();
+        }
+    }
+}
